b286b68fca905fc53a9a8512949ed10329611f59
busybeaver <858259+busybeaver@users.noreply.github.com>
2018-03-10 19:51:12 +0100

    moved packages: ums-connection, ums-id-generator; new package: ums-uuid-generator

--- packages/ums-connection/src/connection.ts
+++ packages/ums-connection/src/connection.ts
@@ -1,16 +1,12 @@
-import { IUserSession } from "../live_engage/session_provider";
-import { Optional } from "../util/types";
-import { IConnectionFactory, IConnectionOpts } from "../transport/connection_factory";
+import { IUserSession } from "@lp-libs/le-session-provider";
+import { Optional } from "@lp-libs/util-types";
+import { IConnectionFactory, IConnectionOpts } from "@lp-libs/transport";
 
 // mark everything optional which has default settings in the default config
-type IConnectionFactoryBase<SessionType extends IUserSession> = Optional<IConnectionFactory<SessionType>>;
+// export type IConnectionFactoryBase<SessionType extends IUserSession> = Optional<IConnectionFactory<SessionType>>;
 
-abstract class BaseWsConnectionFactory<SessionType extends IUserSession> implements IConnectionFactory<SessionType> {
-  public abstract endpoint: (opts: IConnectionOpts<SessionType>) => string;
-
-  constructor(config?: IConnectionFactoryBase<SessionType>) {
-    initConfig(this, config);
-  }
+export abstract class BaseWsConnectionFactory<SessionType extends IUserSession> implements IConnectionFactory<SessionType> {
+  public abstract readonly endpoint: (opts: IConnectionOpts<SessionType>) => string;
 
   public headers = ({ session }: IConnectionOpts<SessionType>): { [key: string]: string } => {
     return {
@@ -20,13 +16,13 @@ abstract class BaseWsConnectionFactory<SessionType extends IUserSession> impleme
 }
 
 export class ConsumerWsConnectionFactory<SessionType extends IUserSession> extends BaseWsConnectionFactory<SessionType> {
-  public endpoint = ({ domain, session }: IConnectionOpts<SessionType>): string => {
+  public readonly endpoint = ({ domain, session }: IConnectionOpts<SessionType>): string => {
     return `wss://${domain}/ws_api/account/${session.accountId}/messaging/consumer?v=3`;
   }
 }
 
 export class AgentWsConnectionFactory<SessionType extends IUserSession> extends BaseWsConnectionFactory<SessionType> {
-  public endpoint = ({ domain, session }: IConnectionOpts<SessionType>): string => {
+  public readonly endpoint = ({ domain, session }: IConnectionOpts<SessionType>): string => {
     // return `wss://${domain}/ws_api/account/${session.accountId}/messaging/brand/${session.token}?v=2`;
     return `wss://${domain}/ws_api/account/${session.accountId}/messaging/brand?v=2`;
   }
--- packages/ums-id-generator/src/generator.ts
+++ packages/ums-id-generator/src/generator.ts
@@ -1,4 +1,4 @@
-export interface IIdGenerator {
+export interface IIDGenerator {
   // generate a random uuid
   generateId: () => string;
 }
@@ -6,7 +6,7 @@ export interface IIdGenerator {
 // source: https://gist.github.com/jed/982883
 // tslint:disable-next-line:no-bitwise
 const generate = (a?) => a ? (a ^ Math.random() * 16 >> a / 4).toString(16) : ("" + 1e7 + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, generate);
-export class DefaultIdGenerator implements IIdGenerator {
+export class DefaultIDGenerator implements IIDGenerator {
   public generateId(): string {
     return generate();
   }
