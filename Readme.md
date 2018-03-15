024798885c6618e6542c3963a8e912f604ee8925
busybeaver <858259+busybeaver@users.noreply.github.com>
2018-03-15 22:54:55 +0100

    test fixes

--- Readme.md
+++ Readme.md
@@ -5,7 +5,6 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/6046092c94b7b4417a64/maintainability)](https://codeclimate.com/github/busybeaver/lp-libs/maintainability)
 [![Snyk Known Vulnerabilities](https://snyk.io/test/github/busybeaver/lp-libs/badge.svg)](https://snyk.io/test/github/busybeaver/lp-libs)
 
-
 WIP: The clients in this repository are in a very early alpha stage. Use with caution, API changes can occur. Pull requests and bug reports are welcome.
 
 ## Disclaimer
--- tests/test-transport.ts
+++ tests/test-transport.ts
@@ -8,7 +8,7 @@ const expect = chai.expect;
 /**
  * Returns the listener count on a given emitter. Used to verify a correct cleanup.
  */
-const listenerCount = e => {
+const listenerCount = (e) => {
   const evts = e.eventNames();
   let count = 0;
   for (let ei = 0; ei < evts.length; ei++) {
@@ -18,44 +18,44 @@ const listenerCount = e => {
 };
 
 describe("Check default usage", () => {
-  it("should send a message with callback", done => {
+  it("should send a message with callback", (done) => {
     const transport = new Transport({
-      wsUrl: "ws://demos.kaazing.com/echo"
+      wsUrl: "ws://demos.kaazing.com/echo",
     });
     const msg = "Hello, there!";
-    transport.on(TransportEvent.ON_ERROR, err => {
+    transport.on(TransportEvent.ON_ERROR, (err) => {
       done(err);
     });
-    transport.sendWithCb(err => {
+    transport.sendWithCb((err) => {
       expect(err).to.be.null;
       transport.shutdown(done);
     }, msg);
   }).timeout(10000);
 
-  it("should send a message and wait for the echo", done => {
+  it("should send a message and wait for the echo", (done) => {
     const transport = new Transport({
-      wsUrl: "ws://demos.kaazing.com/echo" // Important! Make sure this is an echoing endpoint!
+      wsUrl: "ws://demos.kaazing.com/echo", // Important! Make sure this is an echoing endpoint!
     });
     const msg = "Hello, there!";
-    transport.on(TransportEvent.ON_ERROR, err => {
+    transport.on(TransportEvent.ON_ERROR, (err) => {
       done(err);
     });
-    transport.on(TransportEvent.ON_MESSAGE, echo => {
+    transport.on(TransportEvent.ON_MESSAGE, (echo) => {
       expect(echo).to.equal(msg);
       transport.shutdown(done);
     });
-    transport.sendWithCb(err => {
+    transport.sendWithCb((err) => {
       expect(err).to.be.null;
     }, msg);
   }).timeout(20000);
 });
 
 describe("Check statistics", () => {
-  it("should get empty statistics", done => {
+  it("should get empty statistics", (done) => {
     const transport = new Transport({
-      wsUrl: "ws://demos.kaazing.com/echo" // Important! Make sure this is an echoing endpoint!
+      wsUrl: "ws://demos.kaazing.com/echo", // Important! Make sure this is an echoing endpoint!
     });
-    let stats = transport.getStatistics();
+    const stats = transport.getStatistics();
     expect(stats.endpoint).to.equal("ws://demos.kaazing.com/echo");
     expect(stats.errors).to.equal(0);
     expect(stats.outbox).to.equal(0);
@@ -63,10 +63,10 @@ describe("Check statistics", () => {
     expect(stats.sent).to.equal(0);
     done();
   });
-  it.skip("should check correct error counting", done => {
+  it.skip("should check correct error counting", (done) => {
     done("Not implemented yet!");
   });
-  it("should check correct queue and sent counting", done => {
+  it("should check correct queue and sent counting", (done) => {
     const transport = new Transport({
       wsUrl: "ws://demos.kaazing.com/echo", // Important! Make sure this is an echoing endpoint!
     });
@@ -88,9 +88,9 @@ describe("Check statistics", () => {
       done();
     }, 2000/* wait 2 seconds */);
   }).timeout(10000);
-  it("should check correct receive counting", done => {
+  it("should check correct receive counting", (done) => {
     const transport = new Transport({
-      autoConnect: false
+      autoConnect: false,
     });
     transport.pause();
     const iterations = 10;
@@ -100,7 +100,7 @@ describe("Check statistics", () => {
     let stats = transport.getStatistics();
     expect(stats.errors).to.equal(0);
     expect(stats.outbox).to.equal(iterations);
-    transport.shutdown(err => {
+    transport.shutdown((err) => {
       expect(err).to.be.null;
       setTimeout(() => {
         stats = transport.getStatistics();
@@ -113,78 +113,77 @@ describe("Check statistics", () => {
 });
 
 describe("Check wrong usage", () => {
-  it.skip("should check multiple connects", done => {
+  it.skip("should check multiple connects", (done) => {
     done("Not implemented yet!");
   });
-  it.skip("should check multiple discconnects", done => {
+  it.skip("should check multiple discconnects", (done) => {
     done("Not implemented yet!");
   });
 });
 
 describe("Check retry/timeouts", () => {
-  it.skip("should check immediate abort on 0 retries/timeout", done => {
+  it.skip("should check immediate abort on 0 retries/timeout", (done) => {
     done("Not implemented yet!");
   });
 });
 
 describe.skip("Check ConnectionStrategies", () => {
-  describe("Check ConnectionStrategy.LEAVE_OPEN_TIMEOUT", () => {});
-  describe("Check ConnectionStrategy.INDEFINITE", () => {});
+  describe("Check ConnectionStrategy.LEAVE_OPEN_TIMEOUT", () => { });
+  describe("Check ConnectionStrategy.INDEFINITE", () => { });
 });
 
 describe.skip("DEPRECATED Async send message", () => {
-  it("Should send message and close the channel, leaving no event listeners", done => {
-    let aclass = new AATransport({});
+  it("Should send message and close the channel, leaving no event listeners", (done) => {
+    const aclass = new AATransport({});
     aclass
       .connectAsync("ws://demos.kaazing.com/echo")
-      .then(res => aclass.sendAsync("LP rulez"), rej => Promise.reject(rej))
-      .then(res => aclass.disconnectAsync(), rej => Promise.reject(rej))
+      .then((res) => aclass.sendAsync("LP rulez"), (rej) => Promise.reject(rej))
+      .then((res) => aclass.disconnectAsync(), (rej) => Promise.reject(rej))
       .then(
-        res => {
+        (res) => {
           expect(listenerCount(aclass)).to.equal(0);
           done();
         },
-        rej => Promise.reject(rej)
+        (rej) => Promise.reject(rej),
       )
-      .catch(err => {
+      .catch((err) => {
         done(err);
       });
   });
 
-  it("Should not be able to open the channel and fail fast", done => {
-    let aclass = new AATransport({});
+  it("Should not be able to open the channel and fail fast", (done) => {
+    const aclass = new AATransport({});
     aclass
       .connectAsync("ws://dem123os.kaazing.com/echo")
-      .then(res => done("Err: should not get called!"), rej => done())
-      .catch(err => {
+      .then((res) => done("Err: should not get called!"), (rej) => done())
+      .catch((err) => {
         done("Err: " + err);
       });
   });
 
-  it("Should send a message and do open/close immediately", done => {
-    let aclass = new AATransport({
-      connectionStrategy: ConnectionStrategies.CLOSE_IMMEDIATE
+  it("Should send a message and do open/close immediately", (done) => {
+    const aclass = new AATransport({
+      connectionStrategy: ConnectionStrategies.CLOSE_IMMEDIATE,
     });
 
     aclass
       .sendAsync("ws://demos.kaazing.com/echo")
       .then(
-        res => {
+        (res) => {
           expect(listenerCount(aclass)).to.equal(0);
           done();
         },
-        rej => Promise.reject(rej)
+        (rej) => Promise.reject(rej),
       )
-      .catch(err => {
+      .catch((err) => {
         done(err);
       });
   });
 });
 
-
 describe.skip("DEPRECATED Handle multiple connects", () => {
   let t;
-  it("should kill the old ws connection on calling connect again", done => {
+  it("should kill the old ws connection on calling connect again", (done) => {
     let closeCount = 10;
     t = new Transport({});
     for (let i = closeCount; i > 0; i--) {
@@ -192,11 +191,11 @@ describe.skip("DEPRECATED Handle multiple connects", () => {
         expect(code).to.equal(1000); // Normal shutdown
         closeCount--;
       });
-      t.on(t.Event.ON_MESSAGE, rawMsg => {
+      t.on(t.Event.ON_MESSAGE, (rawMsg) => {
         t.disconnect();
       });
       t.on(t.Event.ON_CONNECTED, () => {
-        t.send("Test " + i, err => {
+        t.send("Test " + i, (err) => {
           expect(err).to.be.null;
         });
       });
