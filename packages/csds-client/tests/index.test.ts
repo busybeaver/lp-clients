03fa983611a927a60366b8ff26cf34f214ae8ddc
busybeaver <858259+busybeaver@users.noreply.github.com>
2018-03-16 14:03:14 +0100

    unit tests for csds client and csds config

---
+++ packages/csds-client/tests/index.test.ts
@@ -0,0 +1,13 @@
+import * as index from "../src/index";
+
+test("exposes CsdsClient", () => {
+  expect(index.CsdsClient).toBeDefined();
+});
+
+test("exposes DefaultCsdsClientConfig", () => {
+  expect(index.DefaultCsdsClientConfig).toBeDefined();
+});
+
+test("exposes DEFAULT_CSDS_REFRESH_INTERVAL", () => {
+  expect(index.DEFAULT_CSDS_REFRESH_INTERVAL).toBeGreaterThan(0);
+});
--- packages/csds-config/tests/config.test.ts
+++ packages/csds-config/tests/config.test.ts
@@ -7,33 +7,50 @@ const accountId2 = "le123456";
 const csdsDomain = "foo.bar";
 const service = "testService";
 
-test("DefaultCsdsConfig constructor", () => {
-  const config1 = new DefaultCsdsConfig({ accountId: accountId1, csdsDomain });
-  expect(config1.accountId).toBe(accountId1);
-  expect(config1.csdsDomain).toBe(csdsDomain);
-
-  const config2 = new DefaultCsdsConfig({ accountId: accountId1 });
-  expect(config2.accountId).toBe(accountId1);
-  expect(config2.csdsDomain).toBe(prodDomain);
-
-  const config3 = new DefaultCsdsConfig({ accountId: accountId2 });
-  expect(config3.accountId).toBe(accountId2);
-  expect(config3.csdsDomain).toBe(qaDomain);
+test("init DefaultCsdsConfig", () => {
+  const config = new DefaultCsdsConfig({ accountId: accountId1, csdsDomain });
+  expect(config).toBeInstanceOf(DefaultCsdsConfig);
+  expect(config.accountId).toBe(accountId1);
+  expect(config.csdsDomain).toBe(csdsDomain);
 });
 
-test("exposes DefaultCsdsServiceConfig", () => {
-  const config1 = new DefaultCsdsServiceConfig({ accountId: accountId1, csdsDomain, service });
-  expect(config1.accountId).toBe(accountId1);
-  expect(config1.csdsDomain).toBe(csdsDomain);
-  expect(config1.service).toBe(service);
+test("init DefaultCsdsConfig", () => {
+  const config = new DefaultCsdsConfig({ accountId: accountId1 });
+  expect(config).toBeInstanceOf(DefaultCsdsConfig);
+  expect(config.accountId).toBe(accountId1);
+  expect(config.csdsDomain).toBe(prodDomain);
+});
 
-  const config2 = new DefaultCsdsServiceConfig({ accountId: accountId1, service });
-  expect(config2.accountId).toBe(accountId1);
-  expect(config2.csdsDomain).toBe(prodDomain);
-  expect(config2.service).toBe(service);
+test("init DefaultCsdsConfig", () => {
+  const config = new DefaultCsdsConfig({ accountId: accountId2 });
+  expect(config).toBeInstanceOf(DefaultCsdsConfig);
+  expect(config.accountId).toBe(accountId2);
+  expect(config.csdsDomain).toBe(qaDomain);
+});
 
-  const config3 = new DefaultCsdsServiceConfig({ accountId: accountId2, service });
-  expect(config3.accountId).toBe(accountId2);
-  expect(config3.csdsDomain).toBe(qaDomain);
-  expect(config3.service).toBe(service);
+test("init DefaultCsdsServiceConfig", () => {
+  const config = new DefaultCsdsServiceConfig({ accountId: accountId1, csdsDomain, service });
+  expect(config).toBeInstanceOf(DefaultCsdsConfig);
+  expect(config).toBeInstanceOf(DefaultCsdsServiceConfig);
+  expect(config.accountId).toBe(accountId1);
+  expect(config.csdsDomain).toBe(csdsDomain);
+  expect(config.service).toBe(service);
+});
+
+test("init DefaultCsdsServiceConfig", () => {
+  const config = new DefaultCsdsServiceConfig({ accountId: accountId1, service });
+  expect(config).toBeInstanceOf(DefaultCsdsConfig);
+  expect(config).toBeInstanceOf(DefaultCsdsServiceConfig);
+  expect(config.accountId).toBe(accountId1);
+  expect(config.csdsDomain).toBe(prodDomain);
+  expect(config.service).toBe(service);
+});
+
+test("init DefaultCsdsServiceConfig", () => {
+  const config = new DefaultCsdsServiceConfig({ accountId: accountId2, service });
+  expect(config).toBeInstanceOf(DefaultCsdsConfig);
+  expect(config).toBeInstanceOf(DefaultCsdsServiceConfig);
+  expect(config.accountId).toBe(accountId2);
+  expect(config.csdsDomain).toBe(qaDomain);
+  expect(config.service).toBe(service);
 });
