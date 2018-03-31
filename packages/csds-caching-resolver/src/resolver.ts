9b175343577df09934897fe4e40de171de1c47e1
busybeaver <858259+busybeaver@users.noreply.github.com>
2018-03-31 15:20:23 +0200

    fixes and optimizations for the csds-caching-resolver

--- packages/csds-caching-resolver/src/resolver.ts
+++ packages/csds-caching-resolver/src/resolver.ts
@@ -6,16 +6,10 @@ import { CsdsResponse, ICsdsConfig, ICsdsServiceConfig } from "@lp-libs/csds-con
 export const DEFAULT_CACHE_MAX_AGE: number = 1000 * 60 * 10;
 
 export class CachingCsdsResolver extends SimpleCsdsResolver {
-  private readonly cachedResolve: (options: ICsdsConfig) => Promise<CsdsResponse>;
-
   constructor(maxAge: number = DEFAULT_CACHE_MAX_AGE) {
     super();
     const cacheKey = (options: ICsdsServiceConfig): string =>
       options.service ? `${options.accountId}_${options.service}` : options.accountId;
-    this.cachedResolve = memorize(super.resolve, {maxAge, cacheKey});
-  }
-
-  public resolve(options: ICsdsConfig): Promise<CsdsResponse> {
-    return this.cachedResolve(options);
+    this.resolve = memorize(this.resolve, {maxAge, cacheKey});
   }
 }
