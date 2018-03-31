d57828ceb0225f641d2776df650fe80840ae9530
busybeaver <858259+busybeaver@users.noreply.github.com>
2018-03-31 15:23:13 +0200

    fixes for the MockCsdsResolver

--- packages/csds-resolver/mocks/resolver.ts
+++ packages/csds-resolver/mocks/resolver.ts
@@ -1,4 +1,4 @@
-import { CsdsResponse, ICsdsConfig } from "@lp-libs/csds-config";
+import { CsdsResponse, ICsdsConfig, ICsdsServiceConfig } from "@lp-libs/csds-config";
 import { ICsdsResolver } from "../src/resolver";
 
 export interface IMockCsdsResolver extends ICsdsResolver {
@@ -13,10 +13,10 @@ export const MockCsdsResolver = jest.fn<IMockCsdsResolver>(() => {
       myService: "global.internet.com",
       foo: "bar.com",
     };
-    public resolve = jest.fn<Promise<CsdsResponse>>(() => {
+    public resolve = jest.fn<Promise<CsdsResponse>>((options: ICsdsConfig) => {
       if (this.success) return Promise.resolve(this.response);
       return Promise.reject(new Error("expected failure"));
     });
-    public resolveService = jest.fn<Promise<string>>(() => this.resolve().then((data: CsdsResponse) => data.foo));
+    public resolveService = jest.fn<Promise<string>>((config: ICsdsServiceConfig) => this.resolve(config).then((data: CsdsResponse) => data.foo));
   })();
 });
