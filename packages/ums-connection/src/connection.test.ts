7b8dc406b122372ade18646aca1a59e9378b9d37
busybeaver <858259+busybeaver@users.noreply.github.com>
2018-04-05 00:35:47 +0200

    - removed some duplicate code in some test code

--- packages/ums-connection/src/connection.test.ts
+++ packages/ums-connection/src/connection.test.ts
@@ -1,33 +1,32 @@
 /// <reference types="expect-more-jest" />
 import { MockConnectionOpts } from "@lp-libs/transport/mocks";
+import { IConnectionFactory, IConnectionOpts } from "@lp-libs/transport";
+import { IUserSession } from "@lp-libs/le-session-provider";
 import { AgentWsConnectionFactory, ConsumerWsConnectionFactory, BaseWsConnectionFactory } from "./connection";
+
+const baseChecks = (factory: IConnectionFactory<IUserSession>): IConnectionOpts<IUserSession> => {
+  expect(factory).toBeInstanceOf(BaseWsConnectionFactory);
+
+  const connectionOpts = new MockConnectionOpts();
+  expect(factory.headers).toBeFunction();
+  expect(factory.headers(connectionOpts)).toEqual(expect.objectContaining({ Authorization: expect.stringContaining(connectionOpts.session.token)}));
+
+  expect(factory.endpoint).toBeFunction();
+  expect(factory.endpoint(connectionOpts)).toEqual(expect.stringContaining(connectionOpts.domain));
+  expect(factory.endpoint(connectionOpts)).toEqual(expect.stringContaining(connectionOpts.session.accountId));
+  return connectionOpts;
+};
 
 test("AgentWsConnectionFactory functionality", () => {
   const connectionFactory = new AgentWsConnectionFactory();
-  expect(connectionFactory).toBeInstanceOf(BaseWsConnectionFactory);
+  const connectionOpts = baseChecks(connectionFactory);
   expect(connectionFactory).toBeInstanceOf(AgentWsConnectionFactory);
-
-  const connectionOpts = new MockConnectionOpts();
-  expect(connectionFactory.headers).toBeFunction();
-  expect(connectionFactory.headers(connectionOpts)).toEqual(expect.objectContaining({ Authorization: expect.stringContaining(connectionOpts.session.token)}));
-
-  expect(connectionFactory.endpoint).toBeFunction();
-  expect(connectionFactory.endpoint(connectionOpts)).toEqual(expect.stringContaining(connectionOpts.domain));
-  expect(connectionFactory.endpoint(connectionOpts)).toEqual(expect.stringContaining(connectionOpts.session.accountId));
   expect(connectionFactory.endpoint(connectionOpts)).toEqual(expect.stringContaining("brand"));
 });
 
 test("ConsumerWsConnectionFactory functionality", () => {
   const connectionFactory = new ConsumerWsConnectionFactory();
-  expect(connectionFactory).toBeInstanceOf(BaseWsConnectionFactory);
+  const connectionOpts = baseChecks(connectionFactory);
   expect(connectionFactory).toBeInstanceOf(ConsumerWsConnectionFactory);
-
-  const connectionOpts = new MockConnectionOpts();
-  expect(connectionFactory.headers).toBeFunction();
-  expect(connectionFactory.headers(connectionOpts)).toEqual(expect.objectContaining({ Authorization: expect.stringContaining(connectionOpts.session.token)}));
-
-  expect(connectionFactory.endpoint).toBeFunction();
-  expect(connectionFactory.endpoint(connectionOpts)).toEqual(expect.stringContaining(connectionOpts.domain));
-  expect(connectionFactory.endpoint(connectionOpts)).toEqual(expect.stringContaining(connectionOpts.session.accountId));
   expect(connectionFactory.endpoint(connectionOpts)).toEqual(expect.stringContaining("consumer"));
 });
